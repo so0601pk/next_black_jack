518ec291be585a0e8e41a662e0ba73f122ca08c9
so0601pk <youhami9@gmail.com>
2023-07-05 22:54:32 +0900

    Jestを用いた単体テストの自動化の設定を追加

---
+++ src/app/src/test/unit/component/player.test.ts
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import Player from '@/components/player'
+import { describe } from 'node:test'
+
+describe('Playerコンポーネントのテスト', () => {
+  test('render', () => {})
+})
